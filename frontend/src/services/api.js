18d73b45d7d2820bb9a656a377f9f9b3b04fa2dc
yehao622 <yeahaur@gmail.com>
2026-02-05 11:43:00 -0500

    Use relative API URL for production: guide frontend to communicate with backend on Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001',
+    baseURL: process.env.REACT_APP_API_URL || window.location.origin,
     headers: {
         'Content-Type': 'application/json',
         'X-API-Key': process.env.REACT_APP_API_KEY || ''
